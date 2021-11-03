567829e6f486c8261e210c65ea2ac4bb209c5e6c
s-gabor <ing.gabor@yahoo.com>
2021-11-03 15:23:08 +0200

    Updates

---
+++ precache-manifest.67c01773b8d2473ba120319658684ae9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913decada8a215e17e0fec05bce1c034",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "3d3b3af2fe8f3e973b5e",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/2.cdbe2572.chunk.css"
+  },
+  {
+    "revision": "6fd95a0181e2a07fdbea",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.b9606c73.chunk.css"
+  },
+  {
+    "revision": "3d3b3af2fe8f3e973b5e",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.9e7afc11.chunk.js"
+  },
+  {
+    "revision": "6fd95a0181e2a07fdbea",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.d1a39e7b.chunk.js"
+  },
+  {
+    "revision": "974fe9a407c487a617cb",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.dca84915.js"
+  }
+]);
