c10d730cf0b374f067523f66a2a0a2ae06dea67c
s-gabor <ing.gabor@yahoo.com>
2021-11-03 14:57:29 +0200

    deploy to ropsten

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,9 @@
 const path = require("path");
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const fs = require("fs");
+const secrets = JSON.parse(
+  fs.readFileSync("./secrets/.secrets.json").toString().trim()
+);
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
@@ -6,7 +11,13 @@ module.exports = {
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
     develop: {
-      port: 8545
-    }
-  }
+      port: 8545,
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(secrets.mnemonic, secrets.infuraKey);
+      },
+      network_id: "3",
+    },
+  },
 };
