13e1ea6ba7d41d1aeb03d2d173f7b4b1d65b3b27
s-gabor <ing.gabor@yahoo.com>
2021-11-18 12:54:49 +0200

    Updates

---
+++ precache-manifest.bf11e41d1984998af64b4705b494510c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57b148ead7d6bd67bff86b47b5dc9b7a",
+    "url": "/blockchain-developer-bootcamp-final-project/index.html"
+  },
+  {
+    "revision": "9dfa76f5e3d2d792090c",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/2.cdbe2572.chunk.css"
+  },
+  {
+    "revision": "203110d1ff09939d1ddd",
+    "url": "/blockchain-developer-bootcamp-final-project/static/css/main.2cc095b5.chunk.css"
+  },
+  {
+    "revision": "9dfa76f5e3d2d792090c",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/2.1c4ff662.chunk.js"
+  },
+  {
+    "revision": "203110d1ff09939d1ddd",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/main.729bb138.chunk.js"
+  },
+  {
+    "revision": "974fe9a407c487a617cb",
+    "url": "/blockchain-developer-bootcamp-final-project/static/js/runtime-main.dca84915.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/blockchain-developer-bootcamp-final-project/precache-manifest.67c01773b8d2473ba120319658684ae9.js"
+  "/blockchain-developer-bootcamp-final-project/precache-manifest.bf11e41d1984998af64b4705b494510c.js"
 );
 
 self.addEventListener('message', (event) => {
