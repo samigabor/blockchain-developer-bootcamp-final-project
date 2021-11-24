3e289e9a86ceef7023adc18ae189e62b4150c97c
s-gabor <ing.gabor@yahoo.com>
2021-11-24 11:50:23 +0200

    add smart contract method to deposit extra funds, remove unnecessary methods and comments

--- contracts/Anonymizer.sol
+++ contracts/Anonymizer.sol
@@ -1,14 +1,13 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.5.0 <0.9.0;
+pragma solidity ^0.8.0;
 
+/**
+ * @title App through which a layer of anonymity is added to peer-to-peer transactions
+ * @author Sami Gabor
+ * @notice study project which may have security vulnerabilities
+ */
 contract Anonymizer {
-    /**
-     * @notice Contract owner:
-     * can upgrade contracts
-     * can pause/freeze contracts in case of emergency
-     * can't access users funds
-     **/
     address public owner;
 
     /**
@@ -35,87 +34,53 @@ contract Anonymizer {
 
     /**
      * @notice Returns the ether balance available inside the contract
-     * @return user's contract balance
+     * @return sender's contract balance
      **/
     function getBalance(address _addr) public view returns (uint256) {
         return balances[_addr];
     }
 
     /**
-     * @dev increase the user's contract balance
+     * @notice Deposits funds into the contract and assignes them to the receiver
+     * @dev increase receiver's contract balance
+     * @dev emit the sender's current contract balance
+     * @param _to the destination address to which the ether is assigned
      **/
     function depositEth(address _to) public payable {
         balances[_to] += msg.value;
         emit DepositerEthBalance(balances[msg.sender]);
-        // return balances[msg.sender];
     }
 
     /**
-     * @dev decrease the user's contract balance
-     * @return user's updated balance
+     * @notice Deposits funds into the contract and assignes them to the sender and the receiver
+     * @notice By sending funds to your own address, it increases the transaction anonymity. The funds are kept within the contract and available for claim
+     * @notice DO NOT KEEP LARGE AMOUNTS INTO THE CONTRACT, withdraw them every once in a while
+     * @dev increase the receiver's contract balance
+     * @dev increase the sender's contract balance
+     * @param _to the destination address to which the ether is added
+     * @param _toMyselfAmount the amount assigned back to sender(inside the contract) in order to increase the anonymity
      **/
-    function withdrawEth(address payable _to, uint256 _amount)
-        public
-        returns (uint256)
-    {
+    function depositEth(address _to, uint256 _toMyselfAmount) public payable {
+        require(
+            msg.value > _toMyselfAmount,
+            "The total amount must be greather than the amount deposited back to sender"
+        );
+        uint256 _toAmount = msg.value - _toMyselfAmount;
+        balances[_to] += _toAmount;
+        balances[msg.sender] += _toMyselfAmount;
+        emit DepositerEthBalance(balances[msg.sender]);
+    }
+
+    /**
+     * @notice Withdraw funds, assigned to own address, from the contract
+     * @notice DO NOT KEEP LARGE AMOUNTS INTO THE CONTRACT, withdraw them every once in a while
+     * @dev withwraw from the contract and decrease the sender's balance
+     **/
+    function withdrawEth(address payable _to, uint256 _amount) public {
         require(balances[msg.sender] >= _amount, "Insufficient funds.");
-
-        // the recommanded way to send ether after December 2019(but errors out, probably due to solidity version)
-        // (bool sent, ) = _to.call{value: _amount}("");
-        // require(sent, "Failed to send Ether");
-
         balances[msg.sender] -= _amount;
-        _to.transfer(_amount);
-        // require(sent, "Failed to send Ether");
+        (bool sent, ) = _to.call{value: _amount}("");
+        require(sent, "Failed to send Ether");
         emit EthWithdraw(balances[msg.sender]);
-        return balances[msg.sender];
     }
-
-    /**
-     * @notice Send ether to an address(including yourself)
-     * @notice By sending funds to your own address, the funds are kept within the contract and available for later use
-     * @notice By keeping funds into the contract, it increases the transaction privacy but the risk as well
-     * @notice DO NOT KEEP LARGE AMOUNTS INTO THE CONTRACT
-     * @dev send ether to provided address and/or increase user's contract balance
-     * @param _to the destination address to which the ether is sent
-     * @param _amountToSend the amount of ether sent to the provided address
-     * @param _amountToDeposit the amount of ether to deposit into the contract. Can be zero.
-     * @return user's contract balance
-     **/
-    function sendEther(
-        address _to,
-        uint256 _amountToSend,
-        uint256 _amountToDeposit
-    ) external payable returns (uint256) {}
-
-    /**
-     * @dev increase the user's contract balance
-     * @param _amount the amount to be added to the user's balance
-     * @return user's contract balance
-     **/
-    function depositToInternalAccount(uint256 _amount)
-        private
-        returns (uint256)
-    {}
-
-    /**
-     * @dev decrease user's contract balance
-     * @param _amount the amount to be substracted from the user's balance
-     * @return user's contract balance
-     **/
-    function substractFromInternalAccount(uint256 _amount)
-        private
-        returns (uint256)
-    {}
-
-    /**
-     * @dev send ether to an external account
-     * @param _to the external account address
-     * @param _amount the amount sent to external address
-     * @return transaction success/failure
-     **/
-    function sendToExternalAccount(address _to, uint256 _amount)
-        private
-        returns (bool)
-    {}
 }
