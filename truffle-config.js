30542f3177162c6e26f8db8739715032678ebcf0
s-gabor <ing.gabor@yahoo.com>
2021-11-24 11:48:13 +0200

    add development network, initialize npm

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,10 @@ const path = require("path");
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 const fs = require("fs");
 const secrets = JSON.parse(
-  fs.readFileSync("./secrets/.secrets.json").toString().trim()
+  fs
+    .readFileSync("./secrets/.secrets.json")
+    .toString()
+    .trim()
 );
 
 module.exports = {
@@ -10,11 +13,13 @@ module.exports = {
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
+    development: {
+      host: "127.0.0.1",
       port: 8545,
+      network_id: "*", // Match any network id
     },
     ropsten: {
-      provider: function () {
+      provider: function() {
         return new HDWalletProvider(secrets.mnemonic, secrets.infuraKey);
       },
       network_id: "3",
