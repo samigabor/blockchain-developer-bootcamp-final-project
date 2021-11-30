2c5caa17833216863efe2c71bb2ed6aec5ecd436
s-gabor <ing.gabor@yahoo.com>
2021-12-01 00:30:45 +0200

    sum big numbers using web3.utils, fix wei to eth conversion function

--- client/src/helpers/eth-conversions.js
+++ client/src/helpers/eth-conversions.js
@@ -13,9 +13,9 @@ export function ethToWei(value, web3) {
   return web3.utils.toWei(valueString);
 }
 
-export function sumWeiValues(value1, value2, web3, decimals = 4) {
-  const totalEth = weiToEth(value1, web3) + weiToEth(value2, web3, decimals);
-  return ethToWei(totalEth, web3);
+export function sumWeiValues(value1, value2, web3) {
+  const BN = web3.utils.BN;
+  return new BN(value1).add(new BN(value2));
 }
 
 export function weiToEth(value, web3, decimals = 4) {
@@ -23,7 +23,7 @@ export function weiToEth(value, web3, decimals = 4) {
   if (isNaN(valueString)) {
     return 0;
   }
-  const ethValue = web3.utils.fromWei(valueString);
+  const ethValue = web3.utils.fromWei(value);
   const decimalsValue = 10 ** decimals;
-  return Math.round(ethValue * decimalsValue) / decimalsValue;
+  return Math.floor(ethValue * decimalsValue) / decimalsValue;
 }
